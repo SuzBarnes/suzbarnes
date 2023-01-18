9b7bac6726a8a094aadc31c1ba6856f7b39b0d50
Suzannah Barnes <smgarland@hotmail.co.uk>
2023-01-18 23:55:34 +0000

    updated projects page, added imgs, need to correct css

--- suzbarnes/src/components/NavBar.js
+++ suzbarnes/src/components/NavBar.js
@@ -3,19 +3,20 @@ import { Link } from "react-router-dom";
 import "../styling/navbar.css";
 
 const NavBar = () => {
-  return (
-    <div className="navbar">
-      <Link className="contact" to="contact">
-        <div>Contact Me</div>
-      </Link>
-      <Link className="projects" to="projects">
-        <div>Projects</div>
-      </Link>
-      <Link className="blog" to="blog">
-        <div>Blog</div>
-      </Link>
-    </div>
-  );
+    return (
+        <div className="navbar"><h3>
+            <Link className="contact" to="contact">
+                <div>Contact Me</div>
+            </Link>
+            <Link className="projects" to="projects">
+                <div>Projects</div>
+            </Link>
+            <Link className="blog" to="blog">
+                <div>Blog</div>
+            </Link>
+        </h3>
+        </div>
+    );
 };
 
 export default NavBar;
--- suzbarnes/src/components/Projects.js
+++ suzbarnes/src/components/Projects.js
@@ -2,11 +2,23 @@ import React from "react";
 import "../styling/projects.css";
 
 const Projects = () => {
-  return (
-    <div>
-      <h2>Projects</h2>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Projects</h2>
+            <ul>
+                <li><a href="https://suzbarnes.github.io/virtual-pet">Virtual Pet</a></li>
+                <img src={require('../images/virtual-pet.png')} alt="" />
+                <li><a href="https://suzbarnes.github.io/cruise-ships">Cruise Ships GUI</a></li>
+                <img src={require('../images/cruise-ships.png')} alt="" />
+                <li><a href="https://suzbarnes.github.io/weather-app">Weather App</a></li>
+                <img src={require('../images/weather-app.png')} alt="" />
+                <li><a href="https://youtu.be/CUrmqMA-IB0">Santa Draws</a> - Watch the presentation</li>
+                <img src={require('../images/SantaDraws.png')} alt="" />
+                <li><a href="https://github.com/SuzBarnes/music-library">Music Library Repo</a></li>
+                <li><a href="https://github.com/SuzBarnes/book-library">Book Library Repo</a></li>
+            </ul>
+        </div>
+    );
 };
 
 export default Projects;
