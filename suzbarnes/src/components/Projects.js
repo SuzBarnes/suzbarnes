af02cf4022151c0cf3aff1c5ca3e3c9e38c4cd72
Suzannah Barnes <smgarland@hotmail.co.uk>
2023-01-18 23:21:48 +0000

    setup tests completed, added to contact and home page

---
+++ suzbarnes/src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styling/projects.css";
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>Projects</h2>
+    </div>
+  );
+};
+
+export default Projects;
