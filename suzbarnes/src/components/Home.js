535ab216ee12aa91429bbb33837f4d40a8dc11c8
Suzannah Barnes <smgarland@hotmail.co.uk>
2023-01-18 23:57:57 +0000

    updated projects page, added imgs, need to correct css

--- suzbarnes/src/components/Home.js
+++ suzbarnes/src/components/Home.js
@@ -2,13 +2,21 @@ import React from "react";
 import "../styling/home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>hello I'm Suzannah.</h2>
-            <p>I am a dentist turned developer and after a 6 month part time bootcamp with <a href="https://www.commandshift.co/">Command Shift</a> (MCR codes) I am breaking into the software developer world.</p>
-            <p>Please have a read of my blog to learn more about me and my journey into tech. It was the hardest 6 months of my life, but (I am hoping) well worth it.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h2>hello I'm Suzannah.</h2>
+      <p>
+        I am a dentist turned developer and after a 6 month part time bootcamp
+        with <a href="https://www.commandshift.co/">Command Shift</a> (MCR
+        codes) I am breaking into the software developer world.
+      </p>
+      <p>
+        Please have a read of my blog to learn more about me and my journey into
+        tech. It was the hardest 6 months of my life, but (I am hoping) well
+        worth it.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
