322226234d964ebacbfc2e966b2ff4253a890d8a
Suzannah Barnes <smgarland@hotmail.co.uk>
2023-01-18 23:55:34 +0000

    updated projects page, added imgs, need to correct css

--- suzbarnes/src/components/Blog.js
+++ suzbarnes/src/components/Blog.js
@@ -2,12 +2,12 @@ import React from "react";
 import "../styling/blog.css";
 
 const Blog = () => {
-  return (
-    <div>
-      <h2>Blog</h2>
-      How are you today?
-    </div>
-  );
+    return (
+        <div>
+            <h2>Blog</h2>
+            <h3>Follow my journey here:</h3>
+        </div>
+    );
 };
 
 export default Blog;
--- suzbarnes/src/components/Home.js
+++ suzbarnes/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div>
             <h2>hello I'm Suzannah.</h2>
-            <p>I am a dentist turned developer and after a 6 month part time bootcamp with Command Shift (MCR codes) I am breaking into the software developer world.</p>
+            <p>I am a dentist turned developer and after a 6 month part time bootcamp with <a href="https://www.commandshift.co/">Command Shift</a> (MCR codes) I am breaking into the software developer world.</p>
             <p>Please have a read of my blog to learn more about me and my journey into tech. It was the hardest 6 months of my life, but (I am hoping) well worth it.</p>
         </div>
     );
