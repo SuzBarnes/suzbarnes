0f4308fc25a7983a635dbd50bc011f60b33d21e6
Suzannah Barnes <smgarland@hotmail.co.uk>
2023-01-25 23:40:26 +0000

    updating scss

--- suzbarnes/src/components/NavBarTop.js
+++ suzbarnes/src/components/NavBarTop.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 import "../styling/navbartop.scss";
 
 const NavBarTop = () => {
-    return (
-        <div className="navbar-top">
-            <h3>
-                <Link className="home" to="/">
-                    <div>Home</div>
-                </Link>
-            </h3>
-        </div>
-    );
+  return (
+    <div className="navbar-top">
+      <h3>
+        <Link className="home" to="/">
+          <div>Home</div>
+        </Link>
+      </h3>
+    </div>
+  );
 };
 
 export default NavBarTop;
