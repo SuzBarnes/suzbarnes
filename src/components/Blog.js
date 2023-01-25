c70ad2d0fe9d67761884a84bd18e24ad4224d0c1
Suzannah Barnes <smgarland@hotmail.co.uk>
2023-01-25 23:40:26 +0000

    updating scss

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styling/blog.scss";
+import BlogCard1 from "./BlogCard1";
 
 const Blog = () => {
-    return (
-        <div>
-            <h2>Blog</h2>
-            <h3>Follow my journey here:</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Follow my journey...</h3>
+      <BlogCard1 />
+    </div>
+  );
 };
 
 export default Blog;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,44 +2,39 @@ import React from "react";
 import "../styling/projects.scss";
 
 const Projects = () => {
-    return (
-        <div className="projects">
-            <h2>Projects</h2>
+  return (
+    <div className="projects">
+      <h2>Projects</h2>
 
-            <li>
-                <a href="https://suzbarnes.github.io/virtual-pet">Virtual Pet</a>
-            </li>
-            <img
-                src={require("../images/virtual-pet.png")}
-                alt=""
-            />
-            <li>
-                <a href="https://suzbarnes.github.io/cruise-ships">
-                    Cruise Ships GUI
-                </a>
-            </li>
-            <img src={require("../images/cruise-ships.png")} alt="" />
-            <li>
-                <a href="https://suzbarnes.github.io/weather-app">Weather App</a>
-            </li>
-            <img src={require("../images/weather-app.png")} alt="" />
-            <li>
-                <a href="https://youtu.be/CUrmqMA-IB0">Santa Draws</a> - Watch the
-                presentation
-            </li>
-            <img src={require("../images/SantaDraws.png")} alt="" />
-            <li>
-                <a href="https://github.com/SuzBarnes/music-library">
-                    Music Library Repo
-                </a>
-            </li>
-            <li>
-                <a href="https://github.com/SuzBarnes/book-library">
-                    Book Library Repo
-                </a>
-            </li>
-        </div>
-    );
+      <li>
+        <a href="https://suzbarnes.github.io/virtual-pet">Virtual Pet</a>
+      </li>
+      <img src={require("../images/virtual-pet.png")} alt="" />
+      <li>
+        <a href="https://suzbarnes.github.io/cruise-ships">Cruise Ships GUI</a>
+      </li>
+      <img src={require("../images/cruise-ships.png")} alt="" />
+      <li>
+        <a href="https://suzbarnes.github.io/weather-app">Weather App</a>
+      </li>
+      <img src={require("../images/weather-app.png")} alt="" />
+      <li>
+        <a href="https://youtu.be/CUrmqMA-IB0">Santa Draws</a> - Watch the
+        presentation
+      </li>
+      <img src={require("../images/SantaDraws.png")} alt="" />
+      <li>
+        <a href="https://github.com/SuzBarnes/music-library">
+          Music Library Repo
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/SuzBarnes/book-library">
+          Book Library Repo
+        </a>
+      </li>
+    </div>
+  );
 };
 
 export default Projects;
