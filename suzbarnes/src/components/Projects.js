086efb48bf9dbe6dccf5380a4fa444ebd0412d9e
Suzannah Barnes <smgarland@hotmail.co.uk>
2023-01-26 21:50:14 +0000

    updated projects

--- suzbarnes/src/components/Projects.js
+++ suzbarnes/src/components/Projects.js
@@ -6,24 +6,43 @@ const Projects = () => {
     <div className="projects">
       <h2>Projects</h2>
 
-      <li>
-        <a href="https://suzbarnes.github.io/virtual-pet">Virtual Pet</a>
+      <li className="container">
+        <a href="https://suzbarnes.github.io/virtual-pet">
+          <img src={require("../images/virtual-pet.png")} alt="" />
+          <div className="overlay">
+            <div className="text">Virtual Pet</div>
+          </div>
+        </a>
       </li>
-      <img src={require("../images/virtual-pet.png")} alt="" />
-      <li>
-        <a href="https://suzbarnes.github.io/cruise-ships">Cruise Ships GUI</a>
+      <li className="container">
+        <a href="https://suzbarnes.github.io/cruise-ships">
+          <img src={require("../images/cruise-ships.png")} alt="" />
+          <div className="overlay">
+            <div className="text">Cruise Ship GUI</div>
+          </div>
+        </a>
       </li>
-      <img src={require("../images/cruise-ships.png")} alt="" />
-      <li>
-        <a href="https://suzbarnes.github.io/weather-app">Weather App</a>
+      <li className="container">
+        <a href="https://suzbarnes.github.io/weather-app">
+          <img src={require("../images/weather-app.png")} alt="" />
+          <div className="overlay">
+            <div className="text">Weather App</div>
+          </div>
+        </a>
       </li>
-      <img src={require("../images/weather-app.png")} alt="" />
-      <li>
-        <a href="https://youtu.be/CUrmqMA-IB0">Santa Draws</a> - Watch the
-        presentation
+
+      <li className="container">
+        <a href="https://youtu.be/CUrmqMA-IB0">
+          <img src={require("../images/SantaDraws.png")} alt="" />
+          <div className="overlay">
+            <div className="text">
+              Santa Draws - watch the presentation here!
+            </div>
+          </div>
+        </a>
       </li>
-      <img src={require("../images/SantaDraws.png")} alt="" />
-      <li>
+
+      {/* <li className="container">
         <a href="https://github.com/SuzBarnes/music-library">
           Music Library Repo
         </a>
@@ -32,7 +51,7 @@ const Projects = () => {
         <a href="https://github.com/SuzBarnes/book-library">
           Book Library Repo
         </a>
-      </li>
+      </li> */}
     </div>
   );
 };
